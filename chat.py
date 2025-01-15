0257a54a6360a2cf0c726174590014c548b2f7d4
eniompw <71254710+eniompw@users.noreply.github.com>
2025-01-15 11:19:13 +0000

    concise

--- chat.py
+++ chat.py
@@ -1,14 +1,9 @@
-"""This sample demonstrates a basic call to the chat completion API.
-It is leveraging your endpoint and key. The call is synchronous."""
-
 import os
 from openai import OpenAI
 
 token = os.environ["GITHUB_TOKEN"]
 endpoint = "https://models.inference.ai.azure.com"
-
-# Pick one of the Azure OpenAI models from the GitHub Models service
-model_name = "gpt-4o-mini"
+model_name = "gpt-4o"
 
 client = OpenAI(
     base_url=endpoint,
@@ -24,13 +19,12 @@ response = client.chat.completions.create(
         {
             "role": "user",
             "content": "What is the capital of France?",
-        },
+        }
     ],
-    model=model_name,
-    # Optional parameters
-    temperature=1.,
+    temperature=1.0,
+    top_p=1.0,
     max_tokens=1000,
-    top_p=1.    
+    model=model_name
 )
 
 print(response.choices[0].message.content)
